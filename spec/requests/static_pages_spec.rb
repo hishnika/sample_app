4e3cf4e91af4e59da487bbdb36da4d0e123730ac
Mar Ned <mar.nede@gmail.com>
2013-05-14 14:05:40 -0700

    refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe "Static pages" do
 
   subject { page }
-  let (:base_title) { "Ruby on Rails Tutorial Sample App" }
+  # let (:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
     before { visit root_path }
 
     it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: "#{base_title}") }
+    it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector('title', text: "| Home") }
   end
 
@@ -17,20 +17,20 @@ describe "Static pages" do
     before { visit help_path }
 
     it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: "#{base_title} | Help") }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_selector('h1', text: 'About Us') }   
-    it { should have_selector('title', text: "#{base_title} | About Us") }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_selector('h1', text: 'Contact') }   
-    it { should have_selector('title', text: "#{base_title} | Contact") }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
