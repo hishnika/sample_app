a1f8282e2a014eeb61a37b89c7d6241eccffa07a
Mar Ned <mar.nede@gmail.com>
2013-05-21 17:31:02 -0700

    finish chapter 5

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,2 @@
-def full_title(page_title)
-  base_title = 'Ruby on Rails Tutorial Sample App'
-  if page_title.empty?
-    base_title
-  else
-    # base_title + " | " + page_title
-    "#{base_title} | #{page_title}"
-  end
-end
+# include the full title method in ApplicationHelper to use for tests
+include ApplicationHelper
